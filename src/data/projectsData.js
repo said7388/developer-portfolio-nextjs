6eb54b3afe7f0ef6d50230f885e5af6fded2055b
aztaro97 <aztaro97@gmail.com>
2023-02-24 20:57:35 +0400

    Updated

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,88 +1,80 @@
-import eight from '../assets/svg/projects/eight.svg'
-import five from '../assets/svg/projects/five.svg'
-import four from '../assets/svg/projects/four.svg'
-import one from '../assets/svg/projects/one.svg'
-import seven from '../assets/svg/projects/seven.svg'
-import six from '../assets/svg/projects/six.svg'
-import three from '../assets/svg/projects/three.svg'
-import two from '../assets/svg/projects/two.svg'
-
+import eight from "../assets/png/libft.jpg";
+import five from "../assets/png/fractol.jpg";
+import four from "../assets/png/minitalk.jpg";
+import one from "../assets/png/libft.jpg";
+import seven from "../assets/png/libft.jpg";
+import six from "../assets/png/libft.jpg";
+import three from "../assets/png/get_next_line.png";
+import two from "../assets/png/printf.png";
 
 export const projectsData = [
-    {
-        id: 1,
-        projectName: 'Ayla Networks',
-        projectDesc: 'This project about Simplifying the Development of Smart Home & IoT Solutions.',
-        tags: ['React', 'SAAS', 'React Bootstrap'],
-        code: '',
-        demo: 'https://www.aylanetworks.com/',
-        image: six
-    },
-    {
-        id: 2,
-        projectName: 'Jewelry Niche',
-        projectDesc: 'This project is a Jewelry shop, Customer can be buy product and manage them and make payment.',
-        tags: ['React', 'Express', 'Material Ui'],
-        code: 'https://github.com/said7388/jewelry-niche',
-        demo: 'https://pandora-web.netlify.app/',
-        image: seven
-    },
-    {
-        id: 3,
-        projectName: 'Travel Agency',
-        projectDesc: 'This project about travel agency management.',
-        tags: ['React', 'CSS', 'Material Ui'],
-        code: 'https://github.com/said7388/Travel-Agency-client',
-        demo: 'https://travel-agency-fc58b.web.app/',
-        image: eight
-    },
-    {
-        id: 4,
-        projectName: 'Android Patient Tracker',
-        projectDesc: 'This project involves the development of an Android application for viewing and managing patient data.',
-        tags: ['Flutter', 'Firebase'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: four
-    },
-    {
-        id: 5,
-        projectName: 'E-Commerce App',
-        projectDesc: 'A Simple E-commerce application',
-        tags: ['React Native', 'Firebase'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: five
-    },
-    {
-        id: 6,
-        projectName: 'Uber Lite',
-        projectDesc: 'Uber clone',
-        tags: ['Flutter'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: one
-    },
-    {
-        id: 7,
-        projectName: 'Stock Market App',
-        projectDesc: 'A simple stock market API app',
-        tags: ['React', 'Redux', 'Bootstrap'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: two
-    },
-    {
-        id: 8,
-        projectName: 'Car Pooling System',
-        projectDesc: 'The carpooling system merges multiple people in a car which leads to meet new people, reduces pollution',
-        tags: ['Flutter', 'React'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: three
-    },
-]
-
+  {
+    id: 1,
+    projectName: "LibFt",
+    projectDesc: "Create my own C library",
+    tags: ["C", "Shell"],
+    code: "https://github.com/shatha-jad/Libft",
+    demo: "https://github.com/shatha-jad/Libft",
+    image: one,
+  },
+  {
+    id: 2,
+    projectName: "PrintF",
+    projectDesc: "Replicate the printf function",
+    tags: ["React", "Express", "Material Ui"],
+    code: "https://github.com/shatha-jad/Printf",
+    demo: "https://github.com/shatha-jad/Printf",
+    image: two,
+  },
+  {
+    id: 3,
+    projectName: "Get Next Line",
+    projectDesc:
+      "The aim of this project is to make you code a function that returns a line, read from a file descriptor.",
+    tags: ["React", "CSS", "Material Ui"],
+    code: "https://github.com/shatha-jad/get_next_line",
+    demo: "https://github.com/shatha-jad/get_next_line",
+    image: three,
+  },
+  {
+    id: 4,
+    projectName: "Minitalk",
+    projectDesc:
+      "This project involves the development of an Android application for viewing and managing patient data.",
+    tags: ["Flutter", "Firebase"],
+    code: "https://github.com/shatha-jad/minitalk",
+    demo: "https://github.com/shatha-jad/minitalk",
+    image: four,
+  },
+  {
+    id: 5,
+    projectName: "Fractol",
+    projectDesc: "A Simple E-commerce application",
+    tags: ["React Native", "Firebase"],
+    code: "https://github.com/shatha-jad/fract-ol",
+    demo: "https://github.com/shatha-jad/fract-ol",
+    image: five,
+  },
+  {
+    id: 6,
+    projectName: "Push Wap",
+    projectDesc: `Push swap is a program that sorts different numbers using few instructions to move the numbers with optimized number of moves using two stacks only. The numbers that are allowed to be sorted in this program are between the max int and the minimum int.`,
+    tags: ["Flutter"],
+    code: "https://github.com/shatha-jad/Push__swap",
+    demo: "https://github.com/shatha-jad/Push__swap",
+    image: one,
+  },
+  {
+    id: 7,
+    projectName: "Philosopher",
+    projectDesc:
+      "This project is about solving the philosophers problem who sits all together on a round table and have two forks, one on the right and one on the left, as the number of the forks is the same as the number of philosophers. In addition to that, each philosopher needs to eat with two forks so while some philosophers are eating, the others will be waiting them to finish so they can use the forks.",
+    tags: ["React", "Redux", "Bootstrap"],
+    code: "https://github.com/shatha-jad/philo",
+    demo: "https://github.com/shatha-jad/philo",
+    image: two,
+  },
+];
 
 // Do not remove any fields.
 // Leave it blank instead as shown below
